ced5e5a4a4cd424dcfb45b1c691e766d8d4f2a2c
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-14 12:32:55 +0100

    validations and controllers created

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,3 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :cocktail, presence: true
+  validates :description, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,11 @@ ActiveRecord::Schema.define(version: 2019_11_14_105701) do
   create_table "doses", force: :cascade do |t|
     t.string "description"
     t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_11_14_105701) do
   end
 
   add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
