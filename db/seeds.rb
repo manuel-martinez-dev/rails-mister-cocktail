e2f1ff5494812f94bdd973122532df08394a129f
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-14 17:22:51 +0100

    views created, functionalities all working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_list = open(url).read
+list = JSON.parse(ingredients_list)
+list['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+Cocktail.destroy_all
+
+puts 'Creating COCK tails...'
+
+Cocktail.create!(name: 'White Russian')
+Cocktail.create!(name: 'Long Island Ice Tea')
+Cocktail.create!(name: 'Cuba Libre')
