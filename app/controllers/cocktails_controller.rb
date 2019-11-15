d252b2f865f0aff620195035c9fcf08d23327558
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-15 12:50:04 +0100

    image uploader

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,6 +29,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredients, :description)
+    params.require(:cocktail).permit(:name, :ingredients, :description, :photo)
   end
 end
